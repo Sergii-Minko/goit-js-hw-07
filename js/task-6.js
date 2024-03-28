f02350fbeccdc353693a5373dd37b61326fff962
Sergii Minko <minkosergii@gmail.com>
2024-03-28 16:17:12 +0100

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,43 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+function onBtnElClick() {
+  const amount = document.querySelector('input').value;
+  if (amount >= 1 && amount < 100) {
+    createBoxes(amount);
+  }
+}
+
+function destroyBoxes() {
+  const boxEl = document.querySelector('#boxes');
+  boxEl.innerHTML = '';
+}
+
+function createBox(index) {
+  const newBox = {
+    width: 30 + 10 * (index - 1),
+    height: 30 + 10 * (index - 1),
+    backgroundColor: getRandomHexColor(),
+  };
+  const newBoxElement = document.createElement('div');
+  newBoxElement.style = `width: ${newBox.width}px; height: ${newBox.height}px; background-color: ${newBox.backgroundColor};`;
+  return newBoxElement;
+}
+
+function createBoxes(amount) {
+  const boxesArrayEl = [];
+  const boxEl = document.querySelector('#boxes');
+  boxEl.innerHTML = '';
+  for (let i = 1; i <= amount; i += 1) {
+    boxesArrayEl.push(createBox(i));
+  }
+  boxEl.append(...boxesArrayEl);
+  const amountInput = document.querySelector('input');
+  amountInput.value = '';
+}
+
+const createFocusBtn = document.querySelector('button[data-create]');
+const destroyFocusBtn = document.querySelector('button[data-destroy]');
+
+createFocusBtn.addEventListener('click', onBtnElClick);
+destroyFocusBtn.addEventListener('click', destroyBoxes);
